7943892422560c4148a3b9a2545d555c7abaeee6
Steven <spang22@groton.org>
2020-11-25 18:51:53 -0500

    Virtual Enviroment
    Original Debate Virutal Enviroment

---
+++ Debate Website Virtual Enviroment/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/SPANG22/Desktop/Virtual Enviroments/Debate Website Virtual Enviroment/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
